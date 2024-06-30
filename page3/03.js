e4695b642915c48bd2e9105133ba3abeb106b0d5
staafrrt <staafrrt@gmail.com>
2024-06-30 15:19:57 +0800

    0630漢堡開關完成

--- page3/03.js
+++ page3/03.js
@@ -58,3 +58,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const aside = document.querySelector('aside');
+    const icon = burger.querySelector('.material-icons-round');
+
+    burger.addEventListener('click', function() {
+        aside.classList.toggle('aside-active'); // 切換顯示狀態
+        // burger.classList.toggle('x'); 
+        if (aside.classList.contains('aside-active')) {
+            icon.textContent = 'cancel'; // 切換為cancel icon
+        } else {
+            icon.textContent = 'menu'; // 切換為menu icon
+        }
+    });
+});
